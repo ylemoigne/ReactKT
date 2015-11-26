9cccd96474bc4f3f4a46e40066056219d675bfc7
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2015-11-26 11:58:10 +0300

    Remove redundant declarations

--- subprojects/w3c-dom-html/src/main/kotlin/org/w3c/dom/html/typed/Direction.kt
+++ subprojects/w3c-dom-html/src/main/kotlin/org/w3c/dom/html/typed/Direction.kt
@@ -1,6 +1,6 @@
 package org.w3c.dom.html.typed
 
-enum class Direction(val htmlValue:String) {
+enum class Direction(val htmlValue: String) {
     FORWARD("forward"),
     BACKWARD("backward"),
     NONE("none")
